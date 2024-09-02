ba4b8b91bb1e875bf6d1211e1cdbdc5bf2521d40
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-09-02 11:47:00 +0600

    for of loop basic

---
+++ for_of.js
@@ -0,0 +1,13 @@
+const nums = [12,98,45,63,21,72];
+console.log(nums)
+
+for(const num of nums){
+  console.log(num);
+}
+
+const fruits = ['banana','apple','orange','pineapple'];
+
+for(const fruit of fruits){
+  console.log(fruit)
+  console.log('I want to eat')
+}
---
+++ recap.js
@@ -0,0 +1,9 @@
+// const value will not chnage
+// let value will be chnaged
+
+const amount = 200;
+
+
+//array
+ const nums = [12,13,10,14];
+ 
